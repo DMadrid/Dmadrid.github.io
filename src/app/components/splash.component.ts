38bed61e1ae96db2e93c0825899cfc41c5af0265
Madrid <dem283@cornell.edu>
2020-02-03 23:52:35 -0500

    adjust sizing

--- src/app/components/splash.component.ts
+++ src/app/components/splash.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
     template: `
         <span [@fadeIn]>
             <img src="./assets/images/dm-white.png" style="width: 50%; margin-top: 200px; margin-left: 25%; margin-bottom:1%; margin-right: 25%;">
-            <b style="width: 50%; margin-left: 25%; margin-right: 25%; color: white; width: 100%; font-size: 5.8vw;">DANIEL E. MADRID</b>
+            <b style="width: 49%; margin-left: 25%; margin-right: 25%; color: white; width: 100%; font-size: 5.8vw;">DANIEL E. MADRID</b>
         </span>
     `,
     animations: [
