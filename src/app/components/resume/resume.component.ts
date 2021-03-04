a8bbe5c96cc9ba232b92609da7a55c0d6b1444c5
Madrid <dem283@cornell.edu>
2021-03-03 22:13:10 -0500

    host listener

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,34 +1,49 @@
-import { Component } from '@angular/core';
-// import { MenuItem } from 'primeng-lts/api';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
     selector: 'resume',
     templateUrl: './resume.component.html',
     styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
     baseURL = window.location.origin;
     resumeRef = './assets/documents/dan-madrid-resume.pdf';
     loading = true;
-    zoom = 1;
+    zoom = null;
+    isDesktop = false;
 
     constructor() {}
 
-    increment() {
+    ngOnInit() {
+        if (this.isDesktop) {
+            this.zoom = 1
+        }
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.setIsDesktop()
+    }
+
+    setIsDesktop(): void {
+        this.isDesktop = window.innerWidth > 1000
+    }
+
+    increment(): void {
         if (this.zoom < 2) {
             this.zoom += .1;
         }
     }
 
-    decrement() {
+    decrement(): void {
         if (this.zoom > .5) {
             this.zoom -= .1;
         }
     }
 
-    download() {}
+    download(): void {}
 
-    pdfLoaded() {
+    pdfLoaded(): void {
         this.loading = false
     }
 }
