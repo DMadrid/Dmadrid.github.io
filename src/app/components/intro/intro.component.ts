5d46124bdd64411ec3ee77353fae020fc5cd41de
Madrid <dem283@cornell.edu>
2021-03-05 21:30:26 -0500

    oninit

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'intro',
     templateUrl: './intro.component.html',
     styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit {
 
-    constructor() {
+    ngOnInit(): void {
         this.resizeViewHeight()
     }
 
